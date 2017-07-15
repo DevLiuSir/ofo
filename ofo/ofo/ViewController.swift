f587b3587ac92d94cc0b0fe9f5bd1d8a93ffffc2
ChinaHackers <93428739@qq.com>
2017-07-16 02:14:46 +0800

    初始化项目

---
+++ ofo/ofo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ofo
+//
+//  Created by Liu Chuan on 2017/7/16.
+//  Copyright © 2017年 LC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
